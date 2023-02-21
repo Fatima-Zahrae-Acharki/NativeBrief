58abf419f85815aa99e421816d4e8afff73a885a
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-02-21 13:29:17 +0100

    first commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {Text, View} from 'react-native';
+
+const HelloWorld = () => {
+  return(
+    <View 
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }} >
+    <Text>
+      Hello, World!
+    </Text>
+
+    </View>
+  );
+};
+
+export default HelloWorld;

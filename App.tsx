43e2cecfa29d239cbc6d644b771f91a89e9db268
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-02-21 16:08:02 +0100

    props

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
 import React from "react";
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
-const HelloWorld = () => {
+const styles = StyleSheet.create({
+  center: {
+    alignItems: 'center',
+  },
+});
+
+const Greeting = props => {
   return(
-    <View 
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }} >
-    <Text>
-      Hello, World!
-    </Text>
-
+    <View style ={styles.center}>
+      <Text>Hello {props.name}!</Text>
     </View>
   );
 };
 
-export default HelloWorld;
+const LotsOfGreetings = () => {
+  return(
+   <View style={[styles.center, {top: 50}]}>
+    <Greeting name= "Kirby"></Greeting>
+    <Greeting name= "Django"></Greeting>
+   </View>
+  );
+};
+
+export default LotsOfGreetings;

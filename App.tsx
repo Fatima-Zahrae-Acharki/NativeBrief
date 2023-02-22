a745cf9c879d2f97bfe9201927c7d690c21ac52d
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-02-22 10:13:20 +0100

    counter

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
-import React from "react";
-import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, TouchableOpacity, Text, View} from 'react-native';
 
-const HelloWorld = () => {
-  return(
-    <View 
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }} >
-    <Text>
-      Hello, World!
-    </Text>
+class App extends Component {
+  state = {
+    count: 0,
+  };
 
-    </View>
-  );
-};
+  onPress = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
 
-export default HelloWorld;
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button} onPress={this.onPress}>
+          <Text>Click me</Text>
+        </TouchableOpacity>
+        <View>
+          <Text>You clicked {this.state.count} times</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    marginBottom: 10,
+  },
+});
+
+export default App;

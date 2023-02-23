686e7dee48cc548ea05f090a891d51e8426af276
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-02-23 11:54:18 +0100

    input

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
-import React from "react";
-import {Text, View} from 'react-native';
+import React from 'react';
+import {View, Text, Image, ScrollView, TextInput} from 'react-native';
 
-const HelloWorld = () => {
-  return(
-    <View 
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }} >
-    <Text>
-      Hello, World!
-    </Text>
-
-    </View>
+const App = () => {
+  return (
+    <ScrollView>
+      <Text style={{
+          width:300,
+          flex: 1,
+          alignSelf: 'center',
+        }}>Some text</Text>
+      <View>
+        <Text style={{
+          width:300,
+          flex: 1,
+          alignSelf: 'center',
+        }}>Some more text</Text>
+        <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{width: 200, height: 200}}
+        />
+      </View>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'blue',
+          borderWidth: 1,
+          borderRadius: 20,
+          width:300,
+          alignSelf: 'center',
+        }}
+        defaultValue="You can type here"
+      />
+    </ScrollView>
   );
 };
 
-export default HelloWorld;
+export default App;

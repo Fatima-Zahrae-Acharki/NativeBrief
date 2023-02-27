fb8c255e5ed02dd0c9d7c3e40737e039ae2a44e6
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-02-27 10:06:28 +0100

    function+class components

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import React from "react";
-import {Text, View} from 'react-native';
+import React, { Component } from "react";
+import {Text} from 'react-native';
 
-const HelloWorld = () => {
-  return(
-    <View 
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }} >
-    <Text>
-      Hello, World!
-    </Text>
 
-    </View>
-  );
-};
 
-export default HelloWorld;
+// function component
+
+// const Kirby = () => {
+//   return <Text>Hello, I am Kirby</Text>;
+// };
+
+
+
+//class component
+
+class Kirby extends Component {
+  render() {
+    return <Text>Hello, I am Kirby.</Text>;
+  }
+}
+
+export default Kirby;

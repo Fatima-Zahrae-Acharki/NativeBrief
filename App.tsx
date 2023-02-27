0cd599ab04072315d086b2e221e4eee156a2d5a1
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-02-27 12:34:32 +0100

    state(function/class component)

--- App.tsx
+++ App.tsx
@@ -1,20 +1,73 @@
-import React from "react";
-import {Text, View} from 'react-native';
+import React, {Component, useState} from "react";
+import {Button, Text, View} from 'react-native';
 
-const HelloWorld = () => {
-  return(
-    <View 
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }} >
-    <Text>
-      Hello, World!
-    </Text>
+//functional component
 
-    </View>
-  );
-};
+// const Cat = props => {
+//   const [isHungry, setIsHungry] = useState(true);
 
-export default HelloWorld;
+//   return (
+//     <View>
+//       <Text>
+//         I am {props.name}, and I am {isHungry ? ' hungry' : 'full'} !
+//       </Text>
+//       <Button
+//       onPress={() => {
+//         setIsHungry(false);
+//       }}
+//       disabled={!isHungry}
+//       title={isHungry ? 'pour me some milk, please !' : 'Thank you, nya !'}
+//       />
+//     </View>
+//   );
+// };
+
+// const Cafe = () => {
+//   return (
+//     <>
+//     <Cat name='yuuki' /> 
+//     <Cat name='jiji' /> 
+//     </>
+//   );
+// };
+
+
+
+//class components
+
+class Cat extends Component {
+  state = {isHungry: true};
+
+  render() {
+    return (
+      <View>
+        <Text>
+          I am {this.props.name}, and I am  
+          {this.state.isHungry ? ' hungry' : 'full'} !
+        </Text>
+        <Button
+        onPress={() => {
+          this.setState({isHungry: false});
+        }}
+        disabled={!this.state.isHungry}
+        title={
+          this.state.isHungry ? 'Pour me some milk, please !' : 'Thank you, nya !'
+        }
+        />
+      </View>
+    );
+  }
+
+}
+class Cafe extends Component {
+  render(){
+    return(
+      <>
+      <Cat name='yuuki'/>
+      <Cat name='jiji'/>
+      </>
+    )
+  }
+}
+
+export default Cafe;

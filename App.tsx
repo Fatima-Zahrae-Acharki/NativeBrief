e43249166daeabae61fec404aa5bba30fe063360
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-03-01 13:00:38 +0100

    styles done

--- App.tsx
+++ App.tsx
@@ -1,20 +1,34 @@
 import React from "react";
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
-const HelloWorld = () => {
-  return(
-    <View 
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }} >
-    <Text>
-      Hello, World!
-    </Text>
 
-    </View>
-  );
-};
 
-export default HelloWorld;
+//styles
+
+// const LotsOfStyles = () => {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.pink}>pink color</Text>
+//       <Text style={styles.white}>white color</Text>
+//       <Text style={[styles.pink, styles.white]}>pink, then white</Text>
+//       <Text style={[styles.white, styles.pink]}>white, then pink</Text>
+//     </View>
+//   );
+// };
+
+// const styles=StyleSheet.create({
+//   container: {
+//     marginTop: 200,
+//     alignItems: 'center',
+//   },
+//   white: {
+//     color: 'white',
+//     fontWeight: 'bold',
+//     fontSize: 30,
+//   },
+//   pink: {
+//     color: 'pink',
+//   },
+// });
+
+// export default LotsOfStyles;

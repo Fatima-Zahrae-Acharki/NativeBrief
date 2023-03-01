bb0f91a7aa7f1d4ed65a036a8ce2f573f54dd5ef
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-03-01 13:32:20 +0100

    flex direction

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import {StyleSheet, Text, View} from 'react-native';
+import React, { useState } from "react";
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 
 
@@ -126,31 +126,125 @@ import {StyleSheet, Text, View} from 'react-native';
 //////////////////////////////layout with flexbox
 
 
-//flex
+////////flex
 
-const Flex = () => {
+// const Flex = () => {
+//   return (
+//     <View
+//       style={[
+//         styles.container,
+//         {
+//           //row will appear vertically
+//           // flexDirection: 'row',
+//           flexDirection: 'column',//horizontally
+//         },
+//       ]}>
+//       <View style={{flex: 1, backgroundColor: 'pink'}} />
+//       <View style={{flex: 2, backgroundColor: 'white'}} />
+//       <View style={{flex: 3, backgroundColor: 'powderblue'}} />
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     padding: 20,
+//   },
+// });
+
+// export default Flex;
+
+
+///////flex direction
+
+const FlexDirectionBasics = () => {
+  const [flexDirection, setflexDirection] = useState('column');
+
   return (
-    <View
-      style={[
-        styles.container,
-        {
-          //row will appear vertically
-          // flexDirection: 'row',
-          flexDirection: 'column',//horizontally
-        },
-      ]}>
-      <View style={{flex: 1, backgroundColor: 'pink'}} />
-      <View style={{flex: 2, backgroundColor: 'white'}} />
-      <View style={{flex: 3, backgroundColor: 'powderblue'}} />
-    </View>
+    <PreviewLayout
+      label="flexDirection"
+      values={['column', 'row', 'row-reverse', 'column-reverse']}
+      selectedValue={flexDirection}
+      setSelectedValue={setflexDirection}>
+      <View style={[styles.box, {backgroundColor: 'powderblue'}]} />
+      <View style={[styles.box, {backgroundColor: 'skyblue'}]} />
+      <View style={[styles.box, {backgroundColor: 'steelblue'}]} />
+    </PreviewLayout>
   );
 };
+
+const PreviewLayout = ({
+  label,
+  children,
+  values,
+  selectedValue,
+  setSelectedValue,
+}) => (
+  <View style={{padding: 10, flex: 1}}>
+    <Text style={styles.label}>{label}</Text>
+    <View style={styles.row}>
+      {values.map(value => (
+        <TouchableOpacity
+          key={value}
+          onPress={() => setSelectedValue(value)}
+          style={[styles.button, selectedValue === value && styles.selected]}>
+          <Text
+            style={[
+              styles.buttonLabel,
+              selectedValue === value && styles.selectedLabel,
+            ]}>
+            {value}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+    <View style={[styles.container, {[label]: selectedValue}]}>{children}</View>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    marginTop: 8,
+    backgroundColor: 'aliceblue',
+  },
+  box: {
+    width: 50,
+    height: 50,
+  },
+  row: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  button: {
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 4,
+    backgroundColor: 'oldlace',
+    alignSelf: 'flex-start',
+    marginHorizontal: '1%',
+    marginBottom: 6,
+    minWidth: '48%',
+    textAlign: 'center',
+  },
+  selected: {
+    backgroundColor: 'coral',
+    borderWidth: 0,
+  },
+  buttonLabel: {
+    fontSize: 12,
+    fontWeight: '500',
+    color: 'coral',
+  },
+  selectedLabel: {
+    color: 'white',
+  },
+  label: {
+    textAlign: 'center',
+    marginBottom: 10,
+    fontSize: 24,
   },
 });
 
-export default Flex;
+export default FlexDirectionBasics;

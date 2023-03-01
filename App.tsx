59b7886b512608f9dc343e5e3547022e381c4f2c
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-03-01 12:39:08 +0100

    TypeScript increment decrement example

--- App.tsx
+++ App.tsx
@@ -1,20 +1,67 @@
-import React from "react";
-import {Text, View} from 'react-native';
+import React from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
-const HelloWorld = () => {
-  return(
-    <View 
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }} >
-    <Text>
-      Hello, World!
-    </Text>
 
+
+//increase / decrease typescript example
+export type Props = {
+  name: string;
+  baseEnthusiasmLevel?: number;
+};
+
+const Dattebayo: React.FC<Props> = ({
+  name,
+  baseEnthusiasmLevel = 0,
+}) => {
+  const [enthusiasmLevel, setEnthusiasmLevel] = React.useState(
+    baseEnthusiasmLevel,
+  );
+
+  const onIncrement = () =>
+    setEnthusiasmLevel(enthusiasmLevel + 1);
+  const onDecrement = () =>
+    setEnthusiasmLevel(
+      enthusiasmLevel > 0 ? enthusiasmLevel - 1 : 0,
+    );
+
+  const fishCakes = (numChars: number) =>
+    numChars > 0 ? Array(numChars + 1).join('🍥') : '';
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.greeting}>
+        Dattebayo {name}
+        {fishCakes(enthusiasmLevel)}
+      </Text>
+      <View>
+        <Button
+          title="Increase enthusiasm"
+          accessibilityLabel="increment"
+          onPress={onIncrement}
+          color="pink"
+        />
+        <Button
+          title="Decrease enthusiasm"
+          accessibilityLabel="decrement"
+          onPress={onDecrement}
+          color="powderblue"
+        />
+      </View>
     </View>
   );
 };
 
-export default HelloWorld;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  greeting: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 16,
+  },
+});
+
+export default Dattebayo;

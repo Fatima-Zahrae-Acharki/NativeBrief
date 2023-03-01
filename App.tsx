f53e8a3ea24ffe0f6b8f5c7db677d2f0f35514a7
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-03-01 10:53:36 +0100

    platform specific code/extensions

--- App.tsx
+++ App.tsx
@@ -1,20 +1,81 @@
-import React from "react";
-import {Text, View} from 'react-native';
+//platform module
 
-const HelloWorld = () => {
-  return(
-    <View 
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }} >
-    <Text>
-      Hello, World!
-    </Text>
+// import {Platform, StyleSheet} from 'react-native';
 
-    </View>
-  );
-};
 
-export default HelloWorld;
+// const styles = StyleSheet.create({
+//   height: Platform.OS === 'android' ? 200 : 100,
+// });
+
+
+
+//platform.select method
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     ...Platform.select({
+//       ios: {
+//         backgroundColor: 'red',
+//       },
+//       android: {
+//         backgroundColor: 'green',
+//       },
+//       default: {
+//         // other platforms, web for example
+//         backgroundColor: 'blue',
+//       },
+//     }),
+//   },
+// });
+
+//to return platform-specific components
+
+// const Component = Platform.select({
+//   ios: () => require('ComponentIOS'),
+//   android: () => require('ComponentAndroid'),
+// })();
+
+// <Component />;
+
+//others
+
+// const Component = Platform.select({
+//   native: () => require('ComponentForNative'),
+//   default: () => require('ComponentForWeb'),
+// })();
+
+// <Component />;
+
+
+//Detecting the Android version
+//On Android, the Platform module can also be used to detect the version of the Android Platform in which the app is running:
+
+// import {Platform} from 'react-native';
+
+// if (Platform.Version === 25) {
+//   console.log('Running on Nougat!');
+// }
+
+
+
+//Platform-specific extensions
+
+// BigButton.android.js
+
+//to import the component
+
+// import BigButton from './BigButton';
+
+
+
+//Native-specific extensions (i.e. sharing code with NodeJS and Web)
+
+// Container.js # picked up by Webpack, Rollup or any other Web bundler
+// Container.native.js # picked up by the React Native bundler for both Android and iOS (Metro)
+
+
+//import files without .native
+
+// import Container from './Container';

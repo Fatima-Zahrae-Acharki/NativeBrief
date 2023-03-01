3799a988d5c0b80c9407ed82fc7559f832679ade
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-03-01 13:08:22 +0100

    height and width:fixed dimentions

--- App.tsx
+++ App.tsx
@@ -32,3 +32,37 @@ import {StyleSheet, Text, View} from 'react-native';
 // });
 
 // export default LotsOfStyles;
+
+
+
+//height and width
+//fixed dimentions
+const FixedDimentionsBasics = () => {
+  return (
+    <View>
+         <View
+        style={{
+          width: 50,
+          height: 50,
+          backgroundColor: 'powderblue',
+        }}
+      />
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: 'skyblue',
+        }}
+      />
+      <View
+        style={{
+          width: 150,
+          height: 150,
+          backgroundColor: 'steelblue',
+        }}
+      />
+    </View>
+  );
+};
+
+export default FixedDimentionsBasics;

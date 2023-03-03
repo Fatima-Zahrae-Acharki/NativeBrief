e6077cdcb358076643dae7e178d0d016defce0cd
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-03-03 10:44:09 +0100

    align content layout

--- App.tsx
+++ App.tsx
@@ -556,28 +556,135 @@ import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 //////align self
 
 
-const AlignSelfLayout = () => {
-  const [alignSelf, setAlignSelf] = useState('stretch');
+// const AlignSelfLayout = () => {
+//   const [alignSelf, setAlignSelf] = useState('stretch');
+
+//   return (
+//     <PreviewLayout
+//       label="alignSelf"
+//       selectedValue={alignSelf}
+//       values={['stretch', 'flex-start', 'flex-end', 'center', 'baseline']}
+//       setSelectedValue={setAlignSelf}>
+//       <View
+//         style={[
+//           styles.box,
+//           {
+//             alignSelf,
+//             width: 'auto',
+//             minWidth: 50,
+//             backgroundColor: 'powderblue',
+//           },
+//         ]}
+//       />
+//       <View style={[styles.box, {backgroundColor: 'skyblue'}]} />
+//       <View style={[styles.box, {backgroundColor: 'steelblue'}]} />
+//     </PreviewLayout>
+//   );
+// };
+
+// const PreviewLayout = ({
+//   label,
+//   children,
+//   values,
+//   selectedValue,
+//   setSelectedValue,
+// }) => (
+//   <View style={{padding: 10, flex: 1}}>
+//     <Text style={styles.label}>{label}</Text>
+//     <View style={styles.row}>
+//       {values.map(value => (
+//         <TouchableOpacity
+//           key={value}
+//           onPress={() => setSelectedValue(value)}
+//           style={[styles.button, selectedValue === value && styles.selected]}>
+//           <Text
+//             style={[
+//               styles.buttonLabel,
+//               selectedValue === value && styles.selectedLabel,
+//             ]}>
+//             {value}
+//           </Text>
+//         </TouchableOpacity>
+//       ))}
+//     </View>
+//     <View style={styles.container}>{children}</View>
+//   </View>
+// );
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     marginTop: 8,
+//     backgroundColor: 'aliceblue',
+//     minHeight: 200,
+//   },
+//   box: {
+//     width: 50,
+//     height: 50,
+//   },
+//   row: {
+//     flexDirection: 'row',
+//     flexWrap: 'wrap',
+//   },
+//   button: {
+//     paddingHorizontal: 8,
+//     paddingVertical: 6,
+//     borderRadius: 4,
+//     backgroundColor: 'oldlace',
+//     alignSelf: 'flex-start',
+//     marginHorizontal: '1%',
+//     marginBottom: 6,
+//     minWidth: '48%',
+//     textAlign: 'center',
+//   },
+//   selected: {
+//     backgroundColor: 'coral',
+//     borderWidth: 0,
+//   },
+//   buttonLabel: {
+//     fontSize: 12,
+//     fontWeight: '500',
+//     color: 'coral',
+//   },
+//   selectedLabel: {
+//     color: 'white',
+//   },
+//   label: {
+//     textAlign: 'center',
+//     marginBottom: 10,
+//     fontSize: 24,
+//   },
+// });
+
+// export default AlignSelfLayout;
+
+
+
+///////align content
+
+const AlignContentLayout = () => {
+  const [alignContent, setAlignContent] = useState('flex-start');
 
   return (
     <PreviewLayout
-      label="alignSelf"
-      selectedValue={alignSelf}
-      values={['stretch', 'flex-start', 'flex-end', 'center', 'baseline']}
-      setSelectedValue={setAlignSelf}>
-      <View
-        style={[
-          styles.box,
-          {
-            alignSelf,
-            width: 'auto',
-            minWidth: 50,
-            backgroundColor: 'powderblue',
-          },
-        ]}
-      />
-      <View style={[styles.box, {backgroundColor: 'skyblue'}]} />
-      <View style={[styles.box, {backgroundColor: 'steelblue'}]} />
+      label="alignContent"
+      selectedValue={alignContent}
+      values={[
+        'flex-start',
+        'flex-end',
+        'stretch',
+        'center',
+        'space-between',
+        'space-around',
+      ]}
+      setSelectedValue={setAlignContent}>
+      <View style={[styles.box, {backgroundColor: 'orangered'}]} />
+      <View style={[styles.box, {backgroundColor: 'orange'}]} />
+      <View style={[styles.box, {backgroundColor: 'mediumseagreen'}]} />
+      <View style={[styles.box, {backgroundColor: 'deepskyblue'}]} />
+      <View style={[styles.box, {backgroundColor: 'mediumturquoise'}]} />
+      <View style={[styles.box, {backgroundColor: 'mediumslateblue'}]} />
+      <View style={[styles.box, {backgroundColor: 'purple'}]} />
     </PreviewLayout>
   );
 };
@@ -607,20 +714,21 @@ const PreviewLayout = ({
         </TouchableOpacity>
       ))}
     </View>
-    <View style={styles.container}>{children}</View>
+    <View style={[styles.container, {[label]: selectedValue}]}>{children}</View>
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexWrap: 'wrap',
     marginTop: 8,
     backgroundColor: 'aliceblue',
-    minHeight: 200,
+    maxHeight: 400,
   },
   box: {
     width: 50,
-    height: 50,
+    height: 80,
   },
   row: {
     flexDirection: 'row',
@@ -656,4 +764,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default AlignSelfLayout;
+export default AlignContentLayout;

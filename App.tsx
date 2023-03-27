d318989e1befc9685ee9f08164ee667bfc67ccda
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-03-27 11:42:25 +0200

    flex wrap

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-
+import type { PropsWithChildren } from 'react';
 
 
 /////////////////////////styles
@@ -765,3 +765,109 @@ import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 // });
 
 // export default AlignContentLayout;
+
+
+
+////////flex wrap
+
+const FlexWrapLayout = () => {
+    const [flexWrap, setFlexWrap] = useState('wrap');
+  
+    return (
+      <PreviewLayout
+        label="flexWrap"
+        selectedValue={flexWrap}
+        values={['wrap', 'nowrap']}
+        setSelectedValue={setFlexWrap}>
+        <View style={[styles.box, {backgroundColor: 'orangered'}]} />
+        <View style={[styles.box, {backgroundColor: 'orange'}]} />
+        <View style={[styles.box, {backgroundColor: 'mediumseagreen'}]} />
+        <View style={[styles.box, {backgroundColor: 'deepskyblue'}]} />
+        <View style={[styles.box, {backgroundColor: 'mediumturquoise'}]} />
+        <View style={[styles.box, {backgroundColor: 'mediumslateblue'}]} />
+        <View style={[styles.box, {backgroundColor: 'purple'}]} />
+      </PreviewLayout>
+    );
+  };
+  
+  type PreviewLayoutProps = PropsWithChildren<{
+    label: string;
+    values: string[];
+    selectedValue: string;
+    setSelectedValue: (value: string) => void;
+  }>;
+  
+  const PreviewLayout = ({
+    label,
+    children,
+    values,
+    selectedValue,
+    setSelectedValue,
+  }: PreviewLayoutProps) => (
+    <View style={{padding: 10, flex: 1}}>
+      <Text style={styles.label}>{label}</Text>
+      <View style={styles.row}>
+        {values.map(value => (
+          <TouchableOpacity
+            key={value}
+            onPress={() => setSelectedValue(value)}
+            style={[styles.button, selectedValue === value && styles.selected]}>
+            <Text
+              style={[
+                styles.buttonLabel,
+                selectedValue === value && styles.selectedLabel,
+              ]}>
+              {value}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      <View style={[styles.container, {[label]: selectedValue}]}>{children}</View>
+    </View>
+  );
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: 8,
+      backgroundColor: 'aliceblue',
+      maxHeight: 400,
+    },
+    box: {
+      width: 50,
+      height: 80,
+    },
+    row: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+    },
+    button: {
+      paddingHorizontal: 8,
+      paddingVertical: 6,
+      borderRadius: 4,
+      backgroundColor: 'oldlace',
+      marginHorizontal: '1%',
+      marginBottom: 6,
+      minWidth: '48%',
+      textAlign: 'center',
+    },
+    selected: {
+      backgroundColor: 'coral',
+      borderWidth: 0,
+    },
+    buttonLabel: {
+      fontSize: 12,
+      fontWeight: '500',
+      color: 'coral',
+    },
+    selectedLabel: {
+      color: 'white',
+    },
+    label: {
+      textAlign: 'center',
+      marginBottom: 10,
+      fontSize: 24,
+    },
+  });
+  
+  export default FlexWrapLayout;

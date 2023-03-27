88d655265b1f16fc06c4623bb912b3cd88ab5778
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-03-27 11:47:48 +0200

    Flex Basis, Grow, and Shrink

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
 import type { PropsWithChildren } from 'react';
+import type { ViewStyle } from 'react-native';
 
 
 /////////////////////////styles
@@ -770,104 +771,286 @@ import type { PropsWithChildren } from 'react';
 
 ////////flex wrap
 
-const FlexWrapLayout = () => {
-    const [flexWrap, setFlexWrap] = useState('wrap');
+// const FlexWrapLayout = () => {
+//     const [flexWrap, setFlexWrap] = useState('wrap');
   
-    return (
-      <PreviewLayout
-        label="flexWrap"
-        selectedValue={flexWrap}
-        values={['wrap', 'nowrap']}
-        setSelectedValue={setFlexWrap}>
-        <View style={[styles.box, {backgroundColor: 'orangered'}]} />
-        <View style={[styles.box, {backgroundColor: 'orange'}]} />
-        <View style={[styles.box, {backgroundColor: 'mediumseagreen'}]} />
-        <View style={[styles.box, {backgroundColor: 'deepskyblue'}]} />
-        <View style={[styles.box, {backgroundColor: 'mediumturquoise'}]} />
-        <View style={[styles.box, {backgroundColor: 'mediumslateblue'}]} />
-        <View style={[styles.box, {backgroundColor: 'purple'}]} />
-      </PreviewLayout>
-    );
-  };
+//     return (
+//       <PreviewLayout
+//         label="flexWrap"
+//         selectedValue={flexWrap}
+//         values={['wrap', 'nowrap']}
+//         setSelectedValue={setFlexWrap}>
+//         <View style={[styles.box, {backgroundColor: 'orangered'}]} />
+//         <View style={[styles.box, {backgroundColor: 'orange'}]} />
+//         <View style={[styles.box, {backgroundColor: 'mediumseagreen'}]} />
+//         <View style={[styles.box, {backgroundColor: 'deepskyblue'}]} />
+//         <View style={[styles.box, {backgroundColor: 'mediumturquoise'}]} />
+//         <View style={[styles.box, {backgroundColor: 'mediumslateblue'}]} />
+//         <View style={[styles.box, {backgroundColor: 'purple'}]} />
+//       </PreviewLayout>
+//     );
+//   };
   
-  type PreviewLayoutProps = PropsWithChildren<{
-    label: string;
-    values: string[];
-    selectedValue: string;
-    setSelectedValue: (value: string) => void;
-  }>;
+//   type PreviewLayoutProps = PropsWithChildren<{
+//     label: string;
+//     values: string[];
+//     selectedValue: string;
+//     setSelectedValue: (value: string) => void;
+//   }>;
   
-  const PreviewLayout = ({
-    label,
-    children,
-    values,
-    selectedValue,
-    setSelectedValue,
-  }: PreviewLayoutProps) => (
-    <View style={{padding: 10, flex: 1}}>
-      <Text style={styles.label}>{label}</Text>
-      <View style={styles.row}>
-        {values.map(value => (
-          <TouchableOpacity
-            key={value}
-            onPress={() => setSelectedValue(value)}
-            style={[styles.button, selectedValue === value && styles.selected]}>
-            <Text
-              style={[
-                styles.buttonLabel,
-                selectedValue === value && styles.selectedLabel,
-              ]}>
-              {value}
-            </Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-      <View style={[styles.container, {[label]: selectedValue}]}>{children}</View>
-    </View>
-  );
+//   const PreviewLayout = ({
+//     label,
+//     children,
+//     values,
+//     selectedValue,
+//     setSelectedValue,
+//   }: PreviewLayoutProps) => (
+//     <View style={{padding: 10, flex: 1}}>
+//       <Text style={styles.label}>{label}</Text>
+//       <View style={styles.row}>
+//         {values.map(value => (
+//           <TouchableOpacity
+//             key={value}
+//             onPress={() => setSelectedValue(value)}
+//             style={[styles.button, selectedValue === value && styles.selected]}>
+//             <Text
+//               style={[
+//                 styles.buttonLabel,
+//                 selectedValue === value && styles.selectedLabel,
+//               ]}>
+//               {value}
+//             </Text>
+//           </TouchableOpacity>
+//         ))}
+//       </View>
+//       <View style={[styles.container, {[label]: selectedValue}]}>{children}</View>
+//     </View>
+//   );
   
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      marginTop: 8,
-      backgroundColor: 'aliceblue',
-      maxHeight: 400,
-    },
-    box: {
-      width: 50,
-      height: 80,
-    },
-    row: {
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-    },
-    button: {
-      paddingHorizontal: 8,
-      paddingVertical: 6,
-      borderRadius: 4,
-      backgroundColor: 'oldlace',
-      marginHorizontal: '1%',
-      marginBottom: 6,
-      minWidth: '48%',
-      textAlign: 'center',
-    },
-    selected: {
-      backgroundColor: 'coral',
-      borderWidth: 0,
-    },
-    buttonLabel: {
-      fontSize: 12,
-      fontWeight: '500',
-      color: 'coral',
-    },
-    selectedLabel: {
-      color: 'white',
-    },
-    label: {
-      textAlign: 'center',
-      marginBottom: 10,
-      fontSize: 24,
-    },
-  });
+//   const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       marginTop: 8,
+//       backgroundColor: 'aliceblue',
+//       maxHeight: 400,
+//     },
+//     box: {
+//       width: 50,
+//       height: 80,
+//     },
+//     row: {
+//       flexDirection: 'row',
+//       flexWrap: 'wrap',
+//     },
+//     button: {
+//       paddingHorizontal: 8,
+//       paddingVertical: 6,
+//       borderRadius: 4,
+//       backgroundColor: 'oldlace',
+//       marginHorizontal: '1%',
+//       marginBottom: 6,
+//       minWidth: '48%',
+//       textAlign: 'center',
+//     },
+//     selected: {
+//       backgroundColor: 'coral',
+//       borderWidth: 0,
+//     },
+//     buttonLabel: {
+//       fontSize: 12,
+//       fontWeight: '500',
+//       color: 'coral',
+//     },
+//     selectedLabel: {
+//       color: 'white',
+//     },
+//     label: {
+//       textAlign: 'center',
+//       marginBottom: 10,
+//       fontSize: 24,
+//     },
+//   });
   
-  export default FlexWrapLayout;
+//   export default FlexWrapLayout;
+
+
+
+//////Flex Basis, Grow, and Shrink
+
+
+// const App = () => {
+//     const [powderblue, setPowderblue] = useState<ViewStyle>({
+//       flexGrow: 0,
+//       flexShrink: 1,
+//       flexBasis: 'auto',
+//     });
+//     const [skyblue, setSkyblue] = useState<ViewStyle>({
+//       flexGrow: 1,
+//       flexShrink: 0,
+//       flexBasis: 100,
+//     });
+//     const [steelblue, setSteelblue] = useState<ViewStyle>({
+//       flexGrow: 0,
+//       flexShrink: 1,
+//       flexBasis: 200,
+//     });
+//     return (
+//       <View style={styles.container}>
+//         <View
+//           style={[
+//             styles.container,
+//             {
+//               flexDirection: 'row',
+//               alignContent: 'space-between',
+//             },
+//           ]}>
+//           <BoxInfo color="powderblue" {...powderblue} setStyle={setPowderblue} />
+//           <BoxInfo color="skyblue" {...skyblue} setStyle={setSkyblue} />
+//           <BoxInfo color="steelblue" {...steelblue} setStyle={setSteelblue} />
+//         </View>
+//         <View style={styles.previewContainer}>
+//           <View
+//             style={[
+//               styles.box,
+//               {
+//                 flexBasis: powderblue.flexBasis,
+//                 flexGrow: powderblue.flexGrow,
+//                 flexShrink: powderblue.flexShrink,
+//                 backgroundColor: 'powderblue',
+//               },
+//             ]}
+//           />
+//           <View
+//             style={[
+//               styles.box,
+//               {
+//                 flexBasis: skyblue.flexBasis,
+//                 flexGrow: skyblue.flexGrow,
+//                 flexShrink: skyblue.flexShrink,
+//                 backgroundColor: 'skyblue',
+//               },
+//             ]}
+//           />
+//           <View
+//             style={[
+//               styles.box,
+//               {
+//                 flexBasis: steelblue.flexBasis,
+//                 flexGrow: steelblue.flexGrow,
+//                 flexShrink: steelblue.flexShrink,
+//                 backgroundColor: 'steelblue',
+//               },
+//             ]}
+//           />
+//         </View>
+//       </View>
+//     );
+//   };
+  
+//   type BoxInfoProps = ViewStyle & {
+//     color: string;
+//     setStyle: React.Dispatch<React.SetStateAction<ViewStyle>>;
+//   };
+  
+//   const BoxInfo = ({
+//     color,
+//     flexBasis,
+//     flexShrink,
+//     setStyle,
+//     flexGrow,
+//   }: BoxInfoProps) => (
+//     <View style={[styles.row, {flexDirection: 'column'}]}>
+//       <View
+//         style={[
+//           styles.boxLabel,
+//           {
+//             backgroundColor: color,
+//           },
+//         ]}>
+//         <Text
+//           style={{
+//             color: '#fff',
+//             fontWeight: '500',
+//             textAlign: 'center',
+//           }}>
+//           Box
+//         </Text>
+//       </View>
+//       <Text style={styles.label}>flexBasis</Text>
+//       <TextInput
+//         value={String(flexBasis)}
+//         style={styles.input}
+//         onChangeText={fB =>
+//           setStyle(value => ({
+//             ...value,
+//             flexBasis: isNaN(parseInt(fB, 10)) ? 'auto' : parseInt(fB, 10),
+//           }))
+//         }
+//       />
+//       <Text style={styles.label}>flexShrink</Text>
+//       <TextInput
+//         value={String(flexShrink)}
+//         style={styles.input}
+//         onChangeText={fS =>
+//           setStyle(value => ({
+//             ...value,
+//             flexShrink: isNaN(parseInt(fS, 10)) ? undefined : parseInt(fS, 10),
+//           }))
+//         }
+//       />
+//       <Text style={styles.label}>flexGrow</Text>
+//       <TextInput
+//         value={String(flexGrow)}
+//         style={styles.input}
+//         onChangeText={fG =>
+//           setStyle(value => ({
+//             ...value,
+//             flexGrow: isNaN(parseInt(fG, 10)) ? undefined : parseInt(fG, 10),
+//           }))
+//         }
+//       />
+//     </View>
+//   );
+  
+//   const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       paddingHorizontal: 10,
+//     },
+//     box: {
+//       flex: 1,
+//       height: 50,
+//       width: 50,
+//     },
+//     boxLabel: {
+//       minWidth: 80,
+//       padding: 8,
+//       borderRadius: 4,
+//       marginTop: 8,
+//     },
+//     label: {
+//       marginTop: 6,
+//       fontSize: 16,
+//       fontWeight: '100',
+//     },
+//     previewContainer: {
+//       flex: 1,
+//       flexDirection: 'row',
+//       backgroundColor: 'aliceblue',
+//     },
+//     row: {
+//       flex: 1,
+//       flexDirection: 'row',
+//       flexWrap: 'wrap',
+//       alignItems: 'center',
+//       marginBottom: 10,
+//     },
+//     input: {
+//       borderBottomWidth: 1,
+//       paddingVertical: 3,
+//       width: 50,
+//       textAlign: 'center',
+//     },
+//   });
+  
+//   export default App;

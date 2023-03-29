4a3f51d5dcb23f5fdbc6a3cbd34230713bee8aad
Fatima Zahrae Acharki <fatimazahraeacharki14@gmail.com>
2023-03-29 11:00:42 +0000

    Row Gap, Column Gap and Gap

--- App.tsx
+++ App.tsx
@@ -1054,3 +1054,108 @@ import type { ViewStyle } from 'react-native';
 //   });
   
 //   export default App;
+
+
+
+////////Row Gap, Column Gap and Gap
+
+// const RowGapAndColumnGap = () => {
+//     const [rowGap, setRowGap] = useState(20);
+//     const [columnGap, setColumnGap] = useState(10);
+  
+//     return (
+//       <PreviewLayout
+//         columnGap={columnGap}
+//         handleColumnGapChange={setColumnGap}
+//         rowGap={rowGap}
+//         handleRowGapChange={setRowGap}>
+//         <View style={[styles.box, styles.box1]} />
+//         <View style={[styles.box, styles.box2]} />
+//         <View style={[styles.box, styles.box3]} />
+//         <View style={[styles.box, styles.box4]} />
+//         <View style={[styles.box, styles.box5]} />
+//       </PreviewLayout>
+//     );
+//   };
+  
+//   type PreviewLayoutProps = PropsWithChildren<{
+//     columnGap: number;
+//     handleColumnGapChange: (gap: number) => void;
+//     rowGap: number;
+//     handleRowGapChange: (gap: number) => void;
+//   }>;
+  
+//   const PreviewLayout = ({
+//     children,
+//     handleColumnGapChange,
+//     handleRowGapChange,
+//     rowGap,
+//     columnGap,
+//   }: PreviewLayoutProps) => (
+//     <View style={styles.previewContainer}>
+//       <View style={styles.inputContainer}>
+//         <View style={styles.itemsCenter}>
+//           <Text>Row Gap</Text>
+//           <TextInput
+//             style={styles.input}
+//             value={String(rowGap)}
+//             onChangeText={v => handleRowGapChange(Number(v))}
+//           />
+//         </View>
+//         <View style={styles.itemsCenter}>
+//           <Text>Column Gap</Text>
+//           <TextInput
+//             style={styles.input}
+//             value={String(columnGap)}
+//             onChangeText={v => handleColumnGapChange(Number(v))}
+//           />
+//         </View>
+//       </View>
+//       <View style={[styles.container, {rowGap, columnGap}]}>{children}</View>
+//     </View>
+//   );
+  
+//   const styles = StyleSheet.create({
+//     itemsCenter: {alignItems: 'center'},
+//     inputContainer: {
+//       gap: 4,
+//       flexDirection: 'row',
+//       justifyContent: 'space-around',
+//     },
+//     previewContainer: {padding: 10, flex: 1},
+//     input: {
+//       borderBottomWidth: 1,
+//       paddingVertical: 3,
+//       width: 50,
+//       textAlign: 'center',
+//     },
+//     container: {
+//       flex: 1,
+//       marginTop: 8,
+//       backgroundColor: 'aliceblue',
+//       maxHeight: 400,
+//       flexWrap: 'wrap',
+//       alignContent: 'flex-start',
+//     },
+//     box: {
+//       width: 50,
+//       height: 80,
+//     },
+//     box1: {
+//       backgroundColor: 'orangered',
+//     },
+//     box2: {
+//       backgroundColor: 'orange',
+//     },
+//     box3: {
+//       backgroundColor: 'mediumseagreen',
+//     },
+//     box4: {
+//       backgroundColor: 'deepskyblue',
+//     },
+//     box5: {
+//       backgroundColor: 'mediumturquoise',
+//     },
+//   });
+  
+//   export default RowGapAndColumnGap;
